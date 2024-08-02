af417e38d5d429badb73003177e550187e3d4b2c
JaRP <100147964+Wooloowooloo@users.noreply.github.com>
2024-08-02 14:50:47 +0800

    implemented some audio

--- Assets/_Project/Scriptable Objects/TargetConfigSO.cs
+++ Assets/_Project/Scriptable Objects/TargetConfigSO.cs
@@ -5,13 +5,9 @@ public class TargetConfigSO : ScriptableObject
 {
     [SerializeField] private ETargetShootType _targetShootType;
     [SerializeField] private int _baseScoreValue;
-    [SerializeField] private AudioClip _spawnAudio;
-    [SerializeField] private AudioClip _despawnAudio;
 
     public ETargetShootType TargetShootType { get => _targetShootType; private set => _targetShootType = value; }
     public int BaseScoreValue { get => _baseScoreValue; private set => _baseScoreValue = value;}
-    public AudioClip SpawnAudio { get => _spawnAudio; private set => _spawnAudio = value; }
-    public AudioClip DespawnAudio { get => _despawnAudio; private set => _despawnAudio = value;}
 }
 
 public enum ETargetShootType
