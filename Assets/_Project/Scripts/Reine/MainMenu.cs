dd16a103e1e9ef79e0f840f344fc49879902ff47
RaineCloud <116070381+3rainecloud@users.noreply.github.com>
2024-08-02 18:17:22 +0800

    tried adding main menu sfx

--- Assets/_Project/Scripts/Reine/MainMenu.cs
+++ Assets/_Project/Scripts/Reine/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] string scene_name;
+    [SerializeField] AudioSource _clickSFX;
 
     public void PlayGame()
     {
@@ -15,5 +16,10 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OnClick()
+    {
+        _clickSFX.Play();
     }
 }
